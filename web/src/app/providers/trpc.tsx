bf5db672fedd24ec4304cdbf7d5c80f98ca9ebae
Mansur <vakhabov.mans@gmail.com>
2025-10-02 23:24:57 +0300

    trpc migrate to FSD structure

--- web/src/app/providers/trpc.tsx
+++ web/src/app/providers/trpc.tsx
@@ -1,25 +1,5 @@
-import { createTRPCReact, httpBatchLink } from "@trpc/react-query";
-import type { TrpcRouter } from "@himma-flw/backend/src/trpc";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-export const Trpc = createTRPCReact<TrpcRouter>();
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: false,
-      refetchOnWindowFocus: false,
-    },
-  },
-});
-
-const trpcClient = Trpc.createClient({
-  links: [
-    httpBatchLink({
-      url: "http://localhost:4000/trpc",
-    }),
-  ],
-});
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient, Trpc, trpcClient } from "@/shared/lib/trpc";
 
 export const TrpcProvider = ({ children }: { children: React.ReactNode }) => {
   return (
--- web/src/pages/tasks/ui/TasksPage.tsx
+++ web/src/pages/tasks/ui/TasksPage.tsx
@@ -1,3 +1,21 @@
+import { Trpc } from "@/shared/lib/trpc";
+
 export const TasksPage = () => {
-  return <div>TasksPage</div>;
+  const { data, isLoading, error } = Trpc.getTodos.useQuery();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
+  console.log(data, "ss");
+  return (
+    <div className="bg-amber-300">
+      <h2>Todos</h2>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <ul>
+        {data?.map((t: { id: number; title: string }) => (
+          <li key={t.id}>{t.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
