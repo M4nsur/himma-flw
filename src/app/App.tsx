41529eb9a139049de4e6012e1e2127a3d718144b
Mansur <vakhabov.mans@gmail.com>
2025-10-21 22:00:59 +0300

    remove monorepo structure and exprpess.js

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,10 @@
-import { TrpcProvider } from "@/app/providers/trpc";
 import { AppRouter } from "@/app/routes/";
 import { ThemeProvider } from "@/app/providers/theme";
 
 function App() {
   return (
     <ThemeProvider>
-      <TrpcProvider>
-        <AppRouter />
-      </TrpcProvider>
+      <AppRouter />
     </ThemeProvider>
   );
 }
--- src/pages/tasks/ui/TasksPage.tsx
+++ src/pages/tasks/ui/TasksPage.tsx
@@ -1,19 +1,14 @@
-import { Trpc } from "@/shared/lib/";
 import { PageHeader } from "@/shared/ui/page-header";
 import { AddTaskAction } from "@/features/add-task";
 import { TaskList } from "@/widgets/task-list";
+import { tasks } from "@/shared/values";
 export const TasksPage = () => {
-  const { data, isLoading, error } = Trpc.getTodos.useQuery();
-
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error.message}</div>;
-
   return (
     <div>
       <PageHeader title="Tasks" action={<AddTaskAction />}></PageHeader>
       <div className="flex flex-col">
-        <TaskList variant="active" tasks={data} />
-        <TaskList variant="completed" title="Completed tasks" tasks={data} />
+        <TaskList variant="active" tasks={tasks} />
+        <TaskList variant="completed" title="Completed tasks" tasks={tasks} />
       </div>
     </div>
   );
